bb0aeb96554b148f81373c9c849423bd22322a9c
Gracie Lundell <gracielundell@gmail.com>
2016-03-26 23:04:03 -0700

    style meal editing inputs

--- app/edit-meal-details.component.js
+++ app/edit-meal-details.component.js
@@ -29,7 +29,7 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                     core_1.Component({
                         selector: 'edit-meal-details',
                         inputs: ['meal'],
-                        template: "\n  <div class=\"edit-meal-form\">\n    <h3>Edit Description:</h3>\n    <label>Food:</label>\n    <input [(ngModel)]=\"meal.food\" class=\"meal-edit input-lg\"/> <br>\n    <label>Notes:</label>\n    <input [(ngModel)]=\"meal.notes\" class=\"meal-edit input-lg\"/> <br>\n    <label>Calories:</label>\n    <input [(ngModel)]=\"meal.calories\" class=\"meal-edit input-lg\"/>\n  </div>\n  "
+                        template: "\n  <div class=\"edit-meal-form\">\n    <h3>Edit Description:</h3>\n    <label>Food</label>\n    <input [(ngModel)]=\"meal.food\" class=\"meal-edit\"/> <br>\n    <label>Notes</label>\n    <input [(ngModel)]=\"meal.notes\" class=\"meal-edit\"/> <br>\n    <label>Calories</label>\n    <input [(ngModel)]=\"meal.calories\" class=\"meal-edit\"/>\n  </div>\n  "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], EditMealDetailsComponent);
